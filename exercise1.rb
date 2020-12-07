aadbb1305833ca639e03e75d7a5db5b88d61edcb
BashaM2020 <bashaninetyseven@gmail.com>
2020-12-06 21:05:05 -0500

    finish

--- exercise1.rb
+++ exercise1.rb
@@ -4,3 +4,25 @@ puts 2
 puts 3 
 puts 2 + 3
 
+#strings
+
+puts 'Hello World'
+puts "Hello World"
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop' #escape sequence??????
+puts "1. Hello\n2. World"
+puts "1. Hello 2. World"
+
+#double quotes allow for interpolation
+#String Interpolation
+
+puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+#Booleans, even if one side is True, the whole project will be true
+#&& and
+#|| or
+#! not != not equal
+
+
+
