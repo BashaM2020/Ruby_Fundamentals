ff1bc7e9ca1d42687f75f7b6120f5582c936c4f5
BashaM2020 <bashaninetyseven@gmail.com>
2020-12-07 20:06:33 -0500

    completed exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,33 @@
+# #!!!!!!!!!!!meal cost 55$ + tip
+# #meal_cost= 55
+# #tip=5
+
+# #meal_cost = meal_cost + tip 
+# #meal_cost += tip
+# #puts "The total cost of the meal is " + meal_cost.to_s
+
+# #puts "The result of #{45628 * 7839}"
+
+# #puts (10 < 20 && 30 < 20) || !(10==11)
+#    #!!!!!!!!!!!!T,      F     = T ||not, F= T    =TRUE
+
+# #name= "Sandra"
+# #greeting= "Hello #{name}! It's good to see you again."
+# #mission= "Your mission, should you choose to accept it..."
+
+# #puts "#{greeting} #{mission}"
+
+# #!!!!!!!!!!!!variables and boolean logic
+
+my_number = 12
+your_number = 1
+
+puts my_number > 10
+
+puts my_number < 10
+
+puts my_number == your_number
+
+puts my_number != your_number
+
+#using shorthand with numbers/ 'counter' doing an operation aka a math problem you can change the variable by typing += or -=
