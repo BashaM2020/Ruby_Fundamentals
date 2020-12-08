88b4aebce76a055848c50cbf60fec22759fe6f4f
BashaM2020 <bashaninetyseven@gmail.com>
2020-12-07 22:18:46 -0500

    complete 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,30 @@
-puts "what is your name?"
+# puts "what is your name?"
+
+# user_name = gets.chomp
+
+# puts "Hello, #{user_name}!"
+
+# puts "How old are you?"
+
+# user_age = gets.chomp.to_i
+# current_year = 2020
+
+# puts "Sinc you are #{user_age} years old, this means you were born in #{current_year - user_age}"
+
+#gets vs gets.chomp
+#gets.chomp, gets the user's info and chomps off the extra space/ new line/ enter and just continues the string of info.
+
+#CONTROL STRUCTURES
+
+puts "enter a number for x"
+
+
+x = gets.chomp.to_i
+
+
+
+unless x == 10
+    puts "this isnt a dec :("
+end
+
+
